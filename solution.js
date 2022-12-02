086f9b5423915e6a9d2e63505fa57f16af17949a
karlaferry <karlacarreon.1994@gmail.com>
2022-12-02 20:29:34 +0000

    fix solution

--- solution.js
+++ solution.js
@@ -1,25 +1,27 @@
 const fs = require("fs").promises;
 
 const calculateCalories = () => {
-  getMostCalories().then((caloriesArray) => {
-    console.log(caloriesArray);
+  fs.readFile("./input.txt", "utf8").then((data) => {
+    const caloriesArray = data.split("\n");
     let highestCalories = 0;
     let currentCalories = 0;
     for (let i = 0; i < caloriesArray.length; i++) {
       if (caloriesArray[i].length === 0) {
         currentCalories = 0;
+      } else {
+        currentCalories += +caloriesArray[i];
+        if (currentCalories > highestCalories)
+          highestCalories = currentCalories;
+        caloriesArray[i] = currentCalories;
       }
-      currentCalories += +caloriesArray[i];
-      if (currentCalories > highestCalories) highestCalories = currentCalories;
     }
-    console.log(highestCalories);
-    return highestCalories;
+    const sortedCalories = caloriesArray.sort((a, b) => b - a);
+    console.log(
+      `The highest calorie is ${highestCalories} and the sum of the top 3 is ${
+        sortedCalories[0] + sortedCalories[1] + sortedCalories[2]
+      }.`
+    );
   });
 };
 
-const getMostCalories = () => {
-  return fs.readFile("./input.txt", "utf8").then((data) => {
-    const caloriesArray = data.split("\n");
-    return caloriesArray;
-  });
-};
+console.log(calculateCalories());
